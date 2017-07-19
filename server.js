d23b65a0b2bd70cbdbd9588aedfa9cea988e5fbd
Pavel Ratnikau <p.ratnikau@sdventures.com>
2017-07-20 02:16:00 +0300

    server dependensies added.

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const MongoClient    = require('mongodb').MongoClient;
+const bodyParser     = require('body-parser');
 const app = express()
 app.get('/', (req, res) => {
   res.send('HEY!')
