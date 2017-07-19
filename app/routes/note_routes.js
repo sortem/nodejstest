c59350f3e32039e3eb373b7c4147fe6cfa2b6bff
Pavel Ratnikau <p.ratnikau@sdventures.com>
2017-07-20 02:43:25 +0300

    Get and Put added.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,6 +1,19 @@
+var ObjectID = require('mongodb').ObjectID;
 module.exports = function(app, db) {
+	app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({'error':'An error has occurred'});
+      } else {
+        res.send(item);
+      } 
+    });
+  });
+	
 	app.post('/notes', (req, res) => {
-	const note = { value: req.body.value };
+	const note = { title: req.body.title };
 	db.collection('notes').insert(note, (err, result) => {
 		if (err) {
 			res.send({ 'error': 'An error has occured'});
@@ -10,4 +23,16 @@ module.exports = function(app, db) {
 		}
 	});
   });
+
+	app.put ('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { '_id': new ObjectID(id) };
+    const note = { title: req.body.title };
+    db.collection('notes').update(details, note, (err, result) => {
+      if (err) {
+          res.send({'error':'An error has occurred'});
+      } else {
+          res.send(note);
+      } 
+    });
 };
