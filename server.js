55fa6bbb06e446355c3ada5f6df74a698214066b
Pavel Ratnikau <p.ratnikau@sdventures.com>
2017-07-20 02:59:04 +0300

    json

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const db = require('./config/db');
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 MongoClient.connect(db.url, (err, database) => {
 	if (err) return console.log(err)
 	require('./app/routes')(app, database);
