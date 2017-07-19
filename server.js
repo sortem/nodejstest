f21dc8a96a9cf13f07b0ad183138979f1450b67a
Pavel Ratnikau <p.ratnikau@sdventures.com>
2017-07-20 02:21:20 +0300

    routes added.

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require('express')
 const MongoClient    = require('mongodb').MongoClient;
 const bodyParser     = require('body-parser');
 const app = express()
-app.get('/', (req, res) => {
-  res.send('HEY!')
-})
+
+require('./app/routes')(app, {});
 app.listen(3000, () => console.log('Server running on port 3000'))
