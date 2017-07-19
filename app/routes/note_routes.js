165ce350cf59c9e584d2ab489cd2f2323241f663
Pavel Ratnikau <p.ratnikau@sdventures.com>
2017-07-20 02:55:18 +0300

    console output added.

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -13,6 +13,7 @@ module.exports = function(app, db) {
   });
 
 	app.post('/notes', (req, res) => {
+		    console.log(req.body)
 	const note = { title: req.body };
 	db.collection('notes').insert(note, (err, result) => {
 		if (err) {
